5dead6d88d71793906fc32acb9b00be2f1aa3194
Aakifah-ahmad29 <aakifah.jahangir@appedology.pk>
2025-09-01 23:53:31 -0700

    Props Added

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ function App() {
     <>
       <div>
         <Header />
-        <PropsPract />
+        <PropsPract name="Aakifah " />
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
--- src/components/PropsPract.jsx
+++ src/components/PropsPract.jsx
@@ -1,7 +1,8 @@
-const PropsPract = () => {
+const PropsPract = (props) => {
   return (
     <div>
       <h1>Hi from Props Components</h1>
+      <h2>Name : {props.name}</h2>
     </div>
   );
 };
